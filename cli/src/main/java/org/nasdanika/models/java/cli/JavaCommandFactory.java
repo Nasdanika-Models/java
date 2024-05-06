7aecdfa7846b9025ab017e3c4b6736e5374a6350
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-05 22:51:52 -0400

    Command factory refactoring to completion stage

--- cli/src/main/java/org/nasdanika/models/java/cli/JavaCommandFactory.java
+++ cli/src/main/java/org/nasdanika/models/java/cli/JavaCommandFactory.java
@@ -1,7 +1,11 @@
 package org.nasdanika.models.java.cli;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.BiFunction;
 
+import org.nasdanika.capability.CapabilityProvider;
 import org.nasdanika.cli.RootCommand;
 import org.nasdanika.cli.SubCommandCapabilityFactory;
 import org.nasdanika.common.ProgressMonitor;
@@ -11,9 +15,12 @@ import picocli.CommandLine;
 public class JavaCommandFactory extends SubCommandCapabilityFactory<JavaCommand> {
 
 	@Override
-	protected JavaCommand createCommand(List<CommandLine> parentPath, ProgressMonitor progressMonitor) {
+	protected CompletionStage<JavaCommand> createCommand(
+			List<CommandLine> parentPath, 
+			BiFunction<Object, ProgressMonitor, CompletionStage<Iterable<CapabilityProvider<Object>>>> resolver,
+			ProgressMonitor progressMonitor) {
 		if (parentPath != null && parentPath.size() == 1 && parentPath.get(0).getCommandSpec().userObject() instanceof RootCommand) {
-			return new JavaCommand();			
+			return CompletableFuture.completedStage(new JavaCommand());			
 		}
 		return null;
 	}
